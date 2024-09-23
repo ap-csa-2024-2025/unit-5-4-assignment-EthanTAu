b4917a9b5173a7ad4959493f418ebfa9985a4756
EthanTAu <etau100@student.hbuhsd.edu>
2024-09-23 11:21:00 -0700

    sdag

--- Main.java
+++ Main.java
@@ -9,21 +9,21 @@ public class Main
     System.out.println("Distance: " + result);
 
     //testing quadratic positive solution
-    double a = 1, b = -3, c = 2;
-    double posRoot = posq(a, b, c);
+    double a1 = 1, b1 = -3, c1 = 2;
+    double posRoot = posq(a1, b1, c1);
     System.out.println("Positive root:" + posRoot);
 
     //testing quadratic negative solution
-    double a = 1, b = -3, c = 2); //don't need to type again if keep all in main
-    double negRoot = negq(a, b, c);
+    double a2 = 1, b2 = -3, c2 = 2; //don't need to type again if keep all in main
+    double negRoot = negq(a2, b2, c2);
     System.out.println("Negative root: " + negRoot);
   }
 
   // Implement distance formula method here
   public static double distance(int x1, int y1, int x2, int y2) {
     int fx = x2 - x1;
-    int dy = y2 - y1;
-    return Math.sprt(fx * fy + fy +fy);
+    int fy = y2 - y1;
+    return Math.sprt(fx * fy + fy + fy);
   }
 
   // implement one of your quadratic formula methods here
